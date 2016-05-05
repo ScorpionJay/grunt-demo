e10a6286bab74b976eb941413a6ec9a8ea8aec2a
Jay <jay.wang.scorpion@qq.com>
2016-05-05 21:15:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,45 @@
+# Grunt
+
+##什么是grunt
+JavaScript 世界的构建工具
+
+一句话：自动化。对于需要反复重复的任务，例如压缩（minification）、编译、单元测试、linting等，自动化工具可以减轻你的劳动，简化你的工作。当你在 Gruntfile 文件正确配置好了任务，任务运行器就会自动帮你或你的小组完成大部分无聊的工作。
+##安装
+~~~sh
+// 全局安装
+npm install grunt-cli -g
+// 项目中安装
+npm install grunt --save-dev
+// 查看版本
+grunt --version
+输出
+grunt-cli v1.2.0
+grunt v1.0.1
+~~~
+##使用
+~~~sh
+// 创建gruntfile.js
+touch gruntfile.js
+~~~
+
+    插件
+    grunt-contrib-watch
+    grunt-contrib-connect
+
+
+
+    sass
+    npm install grunt-contrib-sass --save-dev
+    https://www.npmjs.com/package/grunt-contrib-sass
+    需要安装ruby sass
+	This task requires you to have Ruby and Sass installed. 
+
+	http://rubyinstaller.org/downloads/
+	记得勾选add to path
+	ruby -v
+
+	http://www.ruanyifeng.com/blog/2012/06/sass.html
+
+
+
+
---
+++ gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.initConfig({
+
+    connect: {
+      options:{
+        port:9000,
+        hostname:'localhost',
+        livereload:35729
+      },
+      server: {
+        options: {
+          open: true, // auto open default bowser
+          base: ['src']
+        }
+      }
+    },
+
+    watch: {
+      livereload: {
+        options: {
+          livereload: true
+        },
+        files: ['src/{,*/}*.*'],
+      },
+      scripts: {
+         files: [
+                    'src/sass/**/*.scss'
+                ],
+                tasks: ['sass']
+            }
+    },
+
+    // scss
+    sass: { 
+        dist:{                             // Task 
+             files: [{
+                expand: true,
+                cwd: 'src/sass',
+                src: ['*.scss'],
+                dest: 'src/css',
+                ext: '.css'
+              }]
+            }
+        }
+
+
+
+  });
+
+  grunt.registerTask('s', ['sass']);
+
+  grunt.registerTask('serve', [
+    'connect:server',
+    'watch'
+  ]);
+
+};
